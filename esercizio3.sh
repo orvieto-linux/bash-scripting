b14cb304e241b72d81f21bd6e42405e3bdb45bce
Orvieto Linux User Group <orvietolug@gmail.com>
2026-02-12 17:42:59 +0100

    Add script to collect and display user information
    Create a BASH script to gather user data from command line arguments and display it.

---
+++ esercizio3.sh
@@ -0,0 +1,13 @@
+#Creare uno script BASH che chieda all’utente i seguenti dati, stavolta da riga di comando:
+#
+#- Nome
+#- Cognome
+#- Email
+#E li stampi a video come riepilogo, con una frase decisa da te.
+
+#!/usr/bin/env bash
+echo "Nome: $1"
+echo "Cognome: $2"
+Echo "Email: $3"
+
+./script nome cognome email
