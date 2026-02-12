ce3e69c1de1c480aa9c015d9ce123c448b97973d
Orvieto Linux User Group <orvietolug@gmail.com>
2026-02-12 17:38:26 +0100

    Create esercizio1a.sh

---
+++ esercizio1a.sh
@@ -0,0 +1,8 @@
+#Creare uno script BASH che, una volta avviato, mostri a video un messaggio 
+#in cui descrivi come si sta svolgendo la serata finora, con un testo di 
+#massimo 3-4 righe
+
+#!/usr/bin/env bash
+#soluzione banale
+echo "Questa sera abbiamo partecipato all’assemblea dei soci di Orvieto Linux e dopo abbiamo iniziato la prima serata tematica del 2026 sul BASH scripting"
+exit 0
